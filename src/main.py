639f757029b51ccd9e7a514a017f4edbe08588b1
Kyle <krober@thevigorous.com>
2018-06-17 02:33:57 -0500

    add log & load to track already responded to submissions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import datetime
 import os.path
 import sys
 import time
@@ -27,6 +28,8 @@ def get_func(url: str):
 
 class Bot:
     def __init__(self, sub_to_stream: str):
+        self.replied_to = []
+        self.load_replied()
         print(f'initializing on {sub_to_stream}...')
         self.reddit = praw.Reddit()
         self.subreddit = self.reddit.subreddit(sub_to_stream)
@@ -35,9 +38,12 @@ class Bot:
     def run(self):
         print('streaming...')
         for submission in self.subreddit.stream.submissions():
-            print(f'found {submission.title}')
+            print(f'found {submission.id}: {submission.title}')
+            if submission.id in self.replied_to:
+                print('post already replied to')
+                continue
             url = submission.url
-            site_func = get_func(url)
+            site_name, site_func = get_func(url)
             if site_func:
                 print('gathering data...')
                 locations, metadata = site_func(url)
@@ -56,6 +62,26 @@ class Bot:
                 self.rate_limit_handler(e.message)
         else:
             print('replied')
+
+    def log_reply(self, submission, metadata: dict, site_name: str):
+        line = '{id},{mpn},{price},{date},{site}\n'
+        line = line.format(
+            id=submission.id,
+            mpn=metadata.get("mpn"),
+            price=metadata.get("price"),
+            date=datetime.date.today(),
+            site=site_name,
+        )
+        with open('posts.csv', 'a') as f:
+            f.write(line)
+        print('logged')
+
+
+    def load_replied(self):
+        with open('posts.csv', 'r') as f:
+            for line in f:
+                self.replied_to.append(line.split(',')[0])
+        print('loaded')
 
     def rate_limit_handler(self, message: str):
         wait_mins = 1 + [int(c) for c in message.split() if c.isdigit()][0]
