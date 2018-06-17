624cde5de5c77effdf7a178f61d03804c93a178f
Kyle <krober@thevigorous.com>
2018-06-17 18:57:20 -0500

    enable config loading for db

--- src/database/sql_base.py
+++ src/database/sql_base.py
@@ -1,7 +1,28 @@
+import configparser
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('')
+
+def load_engine(config_path: str):
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    db_to_use = config['DEFAULT']['UseDatabase']
+    db_config = config[db_to_use]
+
+    db_type = db_config['DatabaseType']
+    username = db_config['Username']
+    password = db_config['Password']
+    address = db_config['Address']
+    port = db_config['Port']
+
+    conn_string = f'{db_type}://{username}:{password}@{address}:{port}/{username}'
+
+    return create_engine(conn_string)
+
+
+engine = load_engine('./database/db.ini')
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
