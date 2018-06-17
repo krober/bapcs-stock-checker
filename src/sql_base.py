5581b68b9fabf1f2f696ff8147e7ea4d3add77ca
Kyle <krober@thevigorous.com>
2018-06-17 03:06:37 -0500

    add sql_base.py

---
+++ src/sql_base.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
