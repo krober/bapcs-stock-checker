54c866300309d6d9acefb04a436d9de7fc2c1223
Kyle <krober@thevigorous.com>
2018-06-17 21:04:36 -0500

    refactor prints to loggers

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import os.path
 import sys
 import time
@@ -10,6 +11,7 @@ from sqlalchemy import exists
 # fixes sys.argv launching ModuleNotFoundError
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.formatters import formatters
+from src.logger import logger
 from src.stores import microcenter
 from src.models.post import Post
 from src.database.sql_base import SessionMode, session_scope
@@ -21,44 +23,52 @@ class Bot:
     }
 
     def __init__(self, sub_to_stream: str):
-        print(f'initializing on {sub_to_stream}...')
+        self.logger = logger.get_logger('Bot', './logfile.log', logging.DEBUG)
+
+        self.logger.info(f'initializing on {sub_to_stream}...')
         self.reddit = praw.Reddit()
         self.subreddit = self.reddit.subreddit(sub_to_stream)
-        print('initialized')
+        self.logger.info('initialized')
 
     def run(self):
-        print('streaming...')
+        self.logger.info('streaming...')
         for submission in self.subreddit.stream.submissions():
-            print(f'found {submission.id}: {submission.title}')
+            self.logger.info(f'found {submission.id}: {submission.title}')
             if self.already_preplied_to(submission.id):
-                print('post already replied to')
+                self.logger.info('post already replied to')
                 continue
             url = submission.url
             site_name, site_func = self.get_func(url)
             if site_func:
-                print('gathering data...')
+                self.logger.info('gathering data...')
                 inventories, metadata = site_func(url)
                 markdown = formatters.build_markdown(inventories, metadata)
                 self.submit_reply(submission, markdown)
                 self.log_reply(submission, metadata, site_name)
-            else:
-                print('No function mapped to this url')
+            self.logger.info('waiting for next submission...')
+
+    def get_func(self, url: str):
+        for site, func in self.site_funcs.items():
+            if site in url:
+                return site, func
+        self.logger.warning('No function mapped to this url')
+        return None, None
 
     def already_preplied_to(self, submission_id: str):
         with session_scope(SessionMode.READ) as session:
             return session.query(exists().where(Post.reddit_id==submission_id)).scalar()
 
     def submit_reply(self, submission, markdown: str):
-        print('attempting reply...')
+        self.logger.info('attempting reply...')
         try:
             submission.reply(markdown)
         except praw.exceptions.APIException as e:
-            print(e.error_type, e.message)
+            self.logger.error(e.error_type, e.message)
             if e.error_type == 'RATELIMIT':
                 time.sleep(self.get_wait_time(e.message))
                 self.submit_reply(submission, markdown)
         else:
-            print('replied')
+            self.logger.info('replied')
 
     def log_reply(self, submission, metadata: dict, site_name: str):
         post = Post(submission.id,
@@ -68,14 +78,14 @@ class Bot:
                     site_name)
         with session_scope(SessionMode.WRITE) as session:
             session.add(post)
-        print('logged')
+        self.logger.info('written to db')
 
     def get_wait_time(self, message: str):
         if 'seconds' in message:
             wait_mins = 1
         else:
             wait_mins = 1 + [int(c) for c in message.split() if c.isdigit()][0]
-        print(f'Waiting {wait_mins} mins, then restarting...')
+        self.logger.info(f'Waiting {wait_mins} mins, then resubmitting...')
         return wait_mins
 
     def get_func(self, url: str):
