a0aee0cf38b44989362ef95092872f193f2d0bbf
Kyle <krober@thevigorous.com>
2018-06-19 18:10:25 -0500

    add sqlalchemy simple validation to string columns

--- src/models/post.py
+++ src/models/post.py
@@ -1,6 +1,7 @@
 import datetime
 
 from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.orm import validates
 
 from database.sql_base import Base
 
@@ -26,6 +27,20 @@ class Post(Base):
         self.price = price
         self.date = date
         self.site = site
+
+    @validates('mpn', 'site')
+    def validate_lengths(self, key, value):
+        # TODO: add logging of values that violate max_len, likely a parsing error
+        """
+        For attributes in decorator, check against max value len, and truncate if needed
+        :param key: str, passed in from decorator
+        :param value: str, passed in by sqlalchemy
+        :return: str, the shorter of value and value[:30], further handled by sqlalchemy
+        """
+        max_len = getattr(self.__class__, key).prop.columns[0].type.length
+        if value and len(value) > max_len:
+            return value[:max_len]
+        return value
 
     def __repr__(self):
         return f'Post ({self.date} - {self.reddit_id}, {self.mpn})'
