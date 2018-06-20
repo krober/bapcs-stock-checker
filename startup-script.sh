9b9e8347770e90f55f6c9257f7e5723bc478c52b
Kyle <krober@thevigorous.com>
2018-06-20 15:26:47 -0500

    fix bug: round 2: startup-script date format, file ext

--- startup-script.sh
+++ startup-script.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 # get directory of script and cd - should be project root
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $BASE_DIR
+pwd
 
 # start python virtual env
-source ./bin/activate
+source $BASE_DIR/bin/activate
+echo "VENV activated"
 
-cd $DIR/src
+src_dir=$BASE_DIR/src
+cd $src_dir
+pwd
 
 # move current logfile to log-archives, create new logfile
 file_ext=log
 current_name=logfile
 current_time=$(date "+%Y%m%d_%H%M")
-archive_dir=./log-archive
-mkdir -p -- "$backup_dir"
-mv ./$current_name.$file_ext $backup_dir/$current_name-$current_time.$file_ext
+archive_dir=$src_dir/log-archive
+mkdir -p -- "$archive_dir"
+mv $src_dir/$current_name.$file_ext $archive_dir/$current_name-$current_time.$file_ext
 touch $current_name.$file_ext
+
+echo "Logfiles copied/created"
 
 # start program
 # may need to edit python version depending on system
+echo "Starting program..."
 python3.6 main.py
 
 exit 0
