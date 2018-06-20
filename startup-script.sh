b64f1e7782f4c98e31725a109d856b2c3b577ef3
Kyle <krober@thevigorous.com>
2018-06-20 15:18:50 -0500

    fix bug: startup-script date format, file ext

--- startup-script.sh
+++ startup-script.sh
@@ -10,8 +10,8 @@ cd $DIR/src
 
 # move current logfile to log-archives, create new logfile
 file_ext=log
-current_name=logfile.$file_ext
-current_time=$(date "%Y%m%d_%H%M")
+current_name=logfile
+current_time=$(date "+%Y%m%d_%H%M")
 archive_dir=./log-archive
 mkdir -p -- "$backup_dir"
 mv ./$current_name.$file_ext $backup_dir/$current_name-$current_time.$file_ext
