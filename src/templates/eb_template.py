5f81f06833f5e03f65799a156c4d6e52b46e4fb6
Kyle Rober <krober@thevigorous.com>
2018-06-29 11:19:35 -0500

    add seller and product links to ebay template

--- src/templates/eb_template.py
+++ src/templates/eb_template.py
@@ -22,10 +22,20 @@ def get_listing_detail_section(store_data: dict):
     detail_header = f'Item|Notes\n'
     detail_format = f':-:|:-:\n'
 
+    base_seller_url = 'https://www.ebay.com/usr/'
+    base_item_url = 'https://www.ebay.com/itm/'
+
     detail_body = []
     for key, value in store_data.items():
-        if value is not None:
-            detail_body.append(f'{key}|{value}')
+
+        if value is None:
+            continue
+        if key == 'Seller':
+            value = f'[{value}]({base_seller_url + value})'
+        if key == 'Ebay Item Number':
+            value = f'[{value}]({base_item_url + value})'
+
+        detail_body.append(f'{key}|{value}')
 
     detail_body = '\n'.join(detail_body)
 
